068de9558ff172dfb9fb9355bb9113086cdcc0bf
HenryXAG <henryamdrade15@gmail.com>
2026-04-09 22:32:55 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 2 - Actualización de variable en memoria con title y replace de nombre

---
+++ Ejercicio2Parcial2.py
@@ -0,0 +1,12 @@
+nombre_completo = "henry abdiel andrade gomez"
+
+nombre_formato = nombre_completo.title()
+
+# Usamos el reemplazo sobre la variable que ya tiene formato de título
+nuevo_texto = nombre_formato.replace("Henry Abdiel", "Andrade Gomez")
+
+# resultados
+print("RESULTADO EJERCICIO 2")
+print("Nombre original:", nombre_completo)
+print("Formato Título:", nombre_formato)
+print("Texto con Reemplazo:", nuevo_texto)
