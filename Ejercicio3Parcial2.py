b539c0029b22ec58325075250ad44db43c9d172f
HenryXAG <henryamdrade15@gmail.com>
2026-04-09 22:34:45 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 3 - Remoción de prefijo ING con removeprefix y formato upper para normalización

---
+++ Ejercicio3Parcial2.py
@@ -0,0 +1,10 @@
+nombre_ing = "ING. Ever"
+
+# esto remueve el prefijo
+nombre_limpio = nombre_ing.removeprefix("ING. ")
+
+# A mayúsculas
+resultado_final = nombre_limpio.upper()
+
+print("Variable original:", nombre_ing)
+print("Resultado final:", resultado_final)
