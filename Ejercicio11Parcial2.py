19ad3c122b70c252168422226bd5da4d29a7a262
HenryXAG <henryamdrade15@gmail.com>
2026-04-09 22:42:56 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 11 - Formato avanzado con encadenamiento de métodos: strip, title y center

---
+++ Ejercicio11Parcial2.py
@@ -0,0 +1,10 @@
+frase_nido = "  el nido matinal  "
+
+frase_limpia_titulo = frase_nido.strip().title()
+
+resultado_final = frase_limpia_titulo.center(30, "-")
+
+print("RESULTADO EJERCICIO 11")
+print("Original:", frase_nido)
+print("Procesado:", frase_limpia_titulo)
+print("Final:", resultado_final)
