8e49264be5eb36bbeeeb0c972856a3ac7ccfc54a
HenryXAG <henryamdrade15@gmail.com>
2026-04-09 22:41:06 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 9 - Creación de lista de palabras mediante separación por guiones medios usando split

---
+++ Ejercicio9Parcial2.py
@@ -0,0 +1,11 @@
+frase_original = "Any time y Anytime"
+
+# esto reemplaza todas las apariciones de Any time con espacio por Always
+frase_reemplazada = frase_original.replace("Any time", "Always")
+
+resultado_final = frase_reemplazada.upper()
+
+print("RESULTADO EJERCICIO 9")
+print("Texto original:", frase_original)
+print("Después del reemplazo:", frase_reemplazada)
+print("Resultado final (MAYÚSCULAS):", resultado_final)
