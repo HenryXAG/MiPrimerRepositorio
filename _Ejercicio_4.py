b97e55ae5c7968bd8687dd0af70c8de45d90acf6
HenryXAG <henryamdrade15@gmail.com>
2026-04-18 14:37:03 -0600

    Ejercicio 4 - Identificación de números pares e impares utilizando el operador módulo %

---
+++ _Ejercicio_4.py
@@ -0,0 +1,8 @@
+numero = int(input("Ingrese un número para verificar: "))
+
+
+## aqui el operador % nos dice cuánto sobra de la división
+if numero % 2 == 0:
+    print("El número:", numero, "es PAR")
+else:
+    print("El número:", numero, "es IMPAR")
